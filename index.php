f997c5ca77c2df8b86bd25a7cdbdf92bb09e945b
Ranjith-achar <ranjith.chandrachar@gmail.com>
2018-02-26 11:38:55 +0530

    changed variable with publicindex.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 
-class MyClass
+/*class MyClass
 {
     // Class properties and methods go here
 }
@@ -10,6 +10,26 @@ $obj = new MyClass;
 
 var_dump($obj);
 
+?>*/
+
+
+class MyClass
+{
+    // Class properties and methods go here
+
+public $i=5;
+public $sta="sometext";
+
+}
+
+$obj = new MyClass;
+
+var_dump($obj);
+
 ?>
+
+
+
+
 
 ?>
