bac7559d46d7227f79790c4e127633557f0156c7
Ranjith-achar <ranjith.chandrachar@gmail.com>
2018-03-02 20:05:16 +0530

    showing parent class function

---
+++ m_overriding.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class car{
+
+
+ function starting(){
+
+
+  echo "car started with normal";
+
+
+ }
+
+
+
+}
+
+class sportscar extends car{
+
+//now creatinga a function with the same name as parebnt class
+
+
+
+
+}
+
+//now create a object of car
+
+//object of car parent class
+
+$carobject=new sportscar;
+
+$carobject->starting();
+
+
+
+
+
+
+?>
