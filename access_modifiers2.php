b5bf07e9f599236f7c6d3a523941de92c4302aba
Ranjith-thimmachari <ranjith.thimmachari@gmail.com>
2018-03-06 19:43:06 +0530

    commented

--- access_modifiers2.php
+++ access_modifiers2.php
@@ -7,32 +7,30 @@ class food{
   function setprintnme($frname){
 
      $this->fname=$frname;
-
-
-
-  }
+}
 function getprintname(){
 
 echo $this->fname;
 
 }
+}
 
+$foodobj= new food;
+$foodobj->setprintnme("rajith");
+$foodobj->getprintname();
 
+/*
 
+insted of private variale
 
-  }
+it was public
+acees it as directly
 
+objject->propertynam
+e="value ";//assigng value
+ during execution
 
-
-
-  $foodobj= new food;
-  $foodobj->setprintnme("rajith");
-  $foodobj->getprintname();
-
-
-
-
-
+*/
 
 
 ?>
