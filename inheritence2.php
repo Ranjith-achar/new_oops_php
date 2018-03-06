b6f9b504909ebcf6a5df4ca8c3c5d94e5c2760b5
Ranjith-thimmachari <ranjith.thimmachari@gmail.com>
2018-03-06 19:56:33 +0530

    backspace

--- inheritence2.php
+++ inheritence2.php
@@ -9,38 +9,16 @@ echo" speed is 80";
 
 }
 
-
 }
-
-
 
 class UsbFan extends fan{
 
-
-
-
-
 }
-
-
 
 class TableFan extends fan{
 
-
-
-
-
 }
-
-
 //creating object of any child class which calls the method of parent class
-
-
  $UsbFanObj=new UsbFan;
-
  $UsbFanObj->parents();
-
-
-
-
 ?>
